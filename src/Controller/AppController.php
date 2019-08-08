f1dcb3baf2b4023561b197ca1f82df8129ac356f
gopalpatel123 <gopalkrishanp3@gmail.com>
2019-08-08 11:58:50 +0530

    fwwef

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -138,4 +138,51 @@ class AppController extends Controller
 		return $rem;
     	exit;
     }
+	public function currentStockForWeb($item_id)
+    {
+		$this->loadModel('ItemLedgers');
+		//$ItemLedgers=loadModel(ItemLedgers);
+    	//$item_id=$this->request->getData('item_id');
+    	//$variation_id=$this->request->getData('variation_id');
+		//$item_id=1;
+		//$variation_id=2;
+		$transaction_date=date('Y-m-d');
+		//$transaction_date='2019-07-17';
+		
+		$ItemLedgersData = $this->ItemLedgers->find()->select(['item_id','item_variation_id','status','quantity','transaction_date','unit_variation_id'])
+		->contain(['UnitVariations'])
+		->where(['ItemLedgers.transaction_date <= '=>$transaction_date,'item_id'=>$item_id])->autoFields(true);
+		
+		$ItemLedgersData->select(['total_op_qt' => $ItemLedgersData->func()->sum('ItemLedgers.quantity')])
+       ->group(['ItemLedgers.unit_variation_id','ItemLedgers.item_id','ItemLedgers.status',])
+        ;
+		
+		$QuantityTotalStock=0;
+		foreach($ItemLedgersData as $data){
+			if($data->status=="In"){ 
+				//@$QuantityTotalStock[$data->item_id]+=@$data->item_variation->unit_variation->quantity_factor*$data->total_op_qt;
+				@$QuantityTotalStock+=@$data->unit_variation->quantity_factor*$data->total_op_qt;
+			}else{
+				//@$QuantityTotalStock[$data->item_id]-=@$data->item_variation->unit_variation->quantity_factor*$data->total_op_qt;
+				@$QuantityTotalStock-=@$data->unit_variation->quantity_factor*$data->total_op_qt;
+			}
+		}
+		
+		$Orders = $this->ItemLedgers->Orders->find()->contain(['OrderDetails'=>['ItemVariations'=>['UnitVariations']]])->where(['Orders.status NOT IN'=>['Delivered','Cancel']])->toArray();
+		foreach($Orders as $Order){
+			foreach($Order->order_details as $order_detail){
+				if($order_detail->item_id==$item_id){
+				@$QuantityTotalStock-=@$order_detail->item_variation->unit_variation->quantity_factor*$order_detail->quantity;
+				}
+			}
+		}
+		
+		
+		//$ItemVariations=$this->ItemLedgers->ItemVariations->find()->where(['ItemVariations.id'=>$variation_id])->contain(['UnitVariations'])->first();
+		
+		//pr($QuantityTotalStock); pr($ItemVariations->unit_variation->quantity_factor); exit;
+    	$rem=floor($QuantityTotalStock);
+		return $rem;
+    	exit;
+    }
 }
