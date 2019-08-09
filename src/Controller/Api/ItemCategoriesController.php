66a9b0ab13aa2d9f1b682cfeefe5a0dd06705dc1
rohitkumarjoshi <rohitkumarjoshi43@gmail.com>
2019-08-09 12:11:17 +0530

    item category

--- src/Controller/Api/ItemCategoriesController.php
+++ src/Controller/Api/ItemCategoriesController.php
@@ -75,7 +75,7 @@ class ItemCategoriesController extends AppController
                     return $q->where(['ItemVariations.ready_to_sale' =>'Yes']);
                 });
 					
-				$Items = array("title"=>$HomeScreen->title,'layout'=>$HomeScreen->layout,'category_id'=>$HomeScreen->category_id,'item_id'=>$HomeScreen->item_id,'image'=>$HomeScreen->image,'image_fullpath'=>'http://healthymaster.in/healthymaster/img/home_screen/'.$HomeScreen->image,"DynamicList"=>$items);
+				$Items = array("title"=>$HomeScreen->title,'layout'=>$HomeScreen->layout,'category_id'=>$HomeScreen->category_id,'item_id'=>$HomeScreen->item_id,'image'=>$HomeScreen->image,'image_fullpath'=>'http://13.235.146.226/healthymaster/img/home_screen/'.$HomeScreen->image,"DynamicList"=>$items);
 				array_push($dynamic,$Items);
 			}
 			if($HomeScreen->layout=='Category'){
